b2ca469ad247223630423e10a7aeab0d14939398
Truong Minh Khoi Nguyen <truongkhoizz95@gmail.com>
2025-06-17 17:56:20 +0200

    Update Studentdaten-Seite

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -3,8 +3,14 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { getStudentById, addModuleToStudent, removeModuleFromStudent } from '../../../lib/services/student-service';
+import Link from 'next/link';
 import { StudentRead, ModuleCreate } from '@/types/student';
 import styles from '../../styles/Home.module.css';
+import {
+    FaUserGraduate, // Symbol für Student Management
+    FaInfoCircle,   // Symbol für Über Uns / Team
+    FaEnvelope,
+} from 'react-icons/fa';
 
 export default function StudentDetailPage() {
     const { id } = useParams();
@@ -109,101 +115,129 @@ export default function StudentDetailPage() {
     };
 
     return (
-        <div className={styles.studentDetailContainer}>
-            <h1 className={styles.sectionHeading}>Studentendaten</h1>
-
-             {/* Allgemeine Informationen */}
-            <div className={styles.studentInfo}>
-                <p><strong>Vorname:</strong> {student.first_name}</p>
-                <p><strong>Nachname:</strong> {student.last_name}</p>
-                <p><strong>Matrikelnummer:</strong> {student.matriculation_number}</p>
-                <p><strong>Module: </strong></p>
-                {student.modules && student.modules.length > 0 ? (
-                    <ul className={styles.moduleList}>
-                        {student.modules.map((module, index) => (
-                            <li key={index} className={styles.moduleListItem}>
-                                {`${module.code}: ${module.name}`}
-                                <button
-                                    onClick={() => handleRemoveModule(index)}
-                                    className={styles.removeButton}
-                                >
-                                    Entfernen
-                                </button>
+        <div className={styles.homepageContainer}>
+            <nav className={styles.navbar}>
+                    <div className={styles.navbarContainer}>
+                        <div className={styles.navBrand}>
+                            <Link href="/">Student Management</Link>
+                        </div>
+                        <ul className={styles.navbarNav}>
+                            <li className={styles.navItem}>
+                            <Link href="/team" className={styles.navLink}>
+                                <FaInfoCircle className={styles.navIcon} /> About Us
+                            </Link>
                             </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>Keine Module vorhanden.</p>
-                )}
-            </div>
+                        </ul>
+                    </div>
+            </nav>
 
-            {/* Button zum Anzeigen des Formulars */}
-            <div>
-                {!showAddModuleForm && (
-                    <button
-                        onClick={() => setShowAddModuleForm(true)}
-                        className={styles.addModuleButton}
-                    >
-                        + Modul hinzufügen
-                    </button>
-                )}
+            <div className={styles.mainContent}>
+                <h1 className={styles.sectionHeading}>Studentendaten</h1>
 
-                {/* Formular zum Hinzufügen eines Moduls */}
-                {showAddModuleForm && (
-                    <div className={styles.addModuleForm}>
-                        <h2 className={styles.headerModuleForm}>Neues Modul hinzufügen</h2>
-                        <div className="space-y-4">
-                            <div>
-                                <label htmlFor="code" className={styles.formLabel}>
-                                    Modulcode
-                                </label>
-                                <input
-                                    id="code"
-                                    name="code"
-                                    value={newModule.code}
-                                    onChange={(e) => setNewModule((prev) => ({ ...prev, code: e.target.value }))}
-                                    className={styles.formInput}
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="name" className={styles.formLabel}>
-                                    Modulname
-                                </label>
-                                <input
-                                    id="name"
-                                    name="name"
-                                    value={newModule.name}
-                                    onChange={(e) => setNewModule((prev) => ({ ...prev, name: e.target.value }))}
-                                    className={`${styles.formInput} ${styles.formInputWide}`}
-                                />
-                            </div>
-                            {/* Fehleranzeige im Formular */}
-                            {formError && <div className={styles.formError}>{formError}</div>}
-                            
-                            <div className={styles.formButtons}>
-                                <button
-                                    onClick={handleAddModule}
-                                    className={styles.addModuleButton}
-                                >
-                                    Modul hinzufügen
-                                </button>
-                                <button
-                                    onClick={() => {
-                                        setShowAddModuleForm(false);
-                                        setNewModule({ name: '', code: '' }); 
-                                        setFormError(null); 
-                                    }}
-                                    className={styles.cancelButton}
-                                >
-                                    Abbrechen
-                                </button>
+                {/* Allgemeine Informationen */}
+                <div className={styles.studentInfo}>
+                    <p><strong>Vorname:</strong> {student.first_name}</p>
+                    <p><strong>Nachname:</strong> {student.last_name}</p>
+                    <p><strong>Matrikelnummer:</strong> {student.matriculation_number}</p>
+                    <p><strong>Module: </strong></p>
+                    {student.modules && student.modules.length > 0 ? (
+                        <ul className={styles.moduleList}>
+                            {student.modules.map((module, index) => (
+                                <li key={index} className={styles.moduleListItem}>
+                                    {`${module.code}: ${module.name}`}
+                                    <button
+                                        onClick={() => handleRemoveModule(index)}
+                                        className={styles.removeButton}
+                                    >
+                                        Entfernen
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Keine Module vorhanden.</p>
+                    )}
+                    
+                </div>
+
+                {/* Button zum Anzeigen des Formulars */}
+                <div>
+                    {!showAddModuleForm && (
+                        <button
+                            onClick={() => setShowAddModuleForm(true)}
+                            className={styles.addModuleButton}
+                        >
+                            + Modul hinzufügen
+                        </button>
+                    )}
+
+                    {/* Formular zum Hinzufügen eines Moduls */}
+                    {showAddModuleForm && (
+                        <div className={styles.addModuleForm}>
+                            <h2 className={styles.headerModuleForm}>Neues Modul hinzufügen</h2>
+                            <div className="space-y-4">
+                                <div>
+                                    <label htmlFor="code" className={styles.formLabel}>
+                                        Modulcode
+                                    </label>
+                                    <input
+                                        id="code"
+                                        name="code"
+                                        value={newModule.code}
+                                        onChange={(e) => setNewModule((prev) => ({ ...prev, code: e.target.value }))}
+                                        className={styles.formInput}
+                                    />
+                                </div>
+                                <div>
+                                    <label htmlFor="name" className={styles.formLabel}>
+                                        Modulname
+                                    </label>
+                                    <input
+                                        id="name"
+                                        name="name"
+                                        value={newModule.name}
+                                        onChange={(e) => setNewModule((prev) => ({ ...prev, name: e.target.value }))}
+                                        className={`${styles.formInput} ${styles.formInputWide}`}
+                                    />
+                                </div>
+                                {/* Fehleranzeige im Formular */}
+                                {formError && <div className={styles.formError}>{formError}</div>}
+                                
+                                <div className={styles.formButtons}>
+                                    <button
+                                        onClick={handleAddModule}
+                                        className={styles.addModuleButton}
+                                    >
+                                        Modul hinzufügen
+                                    </button>
+                                    <button
+                                        onClick={() => {
+                                            setShowAddModuleForm(false);
+                                            setNewModule({ name: '', code: '' }); 
+                                            setFormError(null); 
+                                        }}
+                                        className={styles.cancelButton}
+                                    >
+                                        Abbrechen
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )}
+                    )}
+                </div>
+                {/* Allgemeiner API-Fehler */}
+                {apiError && <div className={styles.errorMessage}>{apiError}</div>}
             </div>
-            {/* Allgemeiner API-Fehler */}
-            {apiError && <div className={styles.errorMessage}>{apiError}</div>}
+            <footer className={styles.footer}>
+                <div className={styles.footerLinks}>
+                <Link href="/team">
+                    <FaInfoCircle className={styles.footerIcon} /> Team
+                </Link>
+                <Link href="/contact">
+                    <FaEnvelope className={styles.footerIcon} /> Kontakt
+                </Link>
+                </div>
+            </footer>
         </div>
     );
 }
